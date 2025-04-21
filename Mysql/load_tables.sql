196a98e7455a900744c206d882f66d5a8d1381e8
Fer <dba_mex@outlook.com>
2025-04-20 20:47:24 -0600

    Adding mysql files

--- Mysql/load_tables.sql
+++ Mysql/load_tables.sql
@@ -1,34 +1,34 @@
 use imdb;
 
-load data local infile '/home/fer/Downloads/title.akas.tsv' into table akas 
+load data local infile '/home/mysql/Downloads/title.akas.tsv' into table akas 
        fields terminated by '\t' lines terminated by '\n' ;
 
 use imdb;
 
-load data local infile '/home/fer/Downloads/title.basics.tsv' into table basics 
+load data local infile '/home/mysql/Downloads/title.basics.tsv' into table basics 
        fields terminated by '\t' lines terminated by '\n' ;
 
 use imdb;
 
-load data local infile '/home/fer/Downloads/title.crew.tsv' into table crew 
+load data local infile '/home/mysql/Downloads/title.crew.tsv' into table crew 
        fields terminated by '\t' lines terminated by '\n' ;
 
 use imdb;
 
-load data local infile '/home/fer/Downloads/name.basics.tsv' into table name_basics 
+load data local infile '/home/mysql/Downloads/name.basics.tsv' into table name_basics 
        fields terminated by '\t' lines terminated by '\n' ;
 
 use imdb;
 
-load data local infile '/home/fer/Downloads/title.principals.tsv' into table principals 
+load data local infile '/home/mysql/Downloads/title.principals.tsv' into table principals 
        fields terminated by '\t' lines terminated by '\n' ;
 
 use imdb;
 
-load data local infile '/home/fer/Downloads/title.ratings.tsv' into table ratings 
+load data local infile '/home/mysql/Downloads/title.ratings.tsv' into table ratings 
        fields terminated by '\t' lines terminated by '\n' ;
 
 use imdb;
 
-load data local infile '/home/fer/Downloads/title.episode.tsv' into table title_episode 
+load data local infile '/home/mysql/Downloads/title.episode.tsv' into table title_episode 
        fields terminated by '\t' lines terminated by '\n' ;
